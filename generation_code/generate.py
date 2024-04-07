cb5efdad393d862ceb099b1648b68940b2fbf952
Yonatan Bisk <ybisk@yonatanbisk.com>
2024-04-07 14:04:40 -0400

    jinja for loop

--- generation_code/generate.py
+++ generation_code/generate.py
@@ -44,56 +44,7 @@ def pretty_author_names(s):
     s = s[1:-1]
   return s
 
-def publication_html(entry):
-    template_string = """
-    <div class="card {{ entry['FIELD'] }}">
-      <div class="card-body" 
-          style="background-color: {{ '#FFFFFF' if entry['TYPE'] in ['journal', 'conference'] else '#EBEBEB' }};">
-        <div class="row">
-          <div class="col-md-1">
-            <button type="button" disabled 
-                    class="btn ybtn btn-outline-{{ colors[entry['FIELD']] }}">{{ entry['YEAR'] }}</button>
-              {{ entry['STUDENTPHOTO'] | safe }}
-          </div>
-          <div class="col-md-11">
-            <b style="font-weight: 500; font-size:15pt" >
-                {{ entry['TITLE'] }}
-            </b><br>
-            <div class="card-text">
-              <i style="font-weight: 300; font-size:11pt">
-                {{ entry['VENUE'] }} ({{ entry['VENUE-ACR'] }})
-              </i><br>
-              <font style="font-weight: 300; font-size:11pt">
-                {{ entry['AUTHORS'] }}
-              </font><br>
-              {{ entry['PAPER-BUTTON'] | safe }}
-              <a class="sbtn" title
-                  data-original-title="Copy BibTex" id="buttonidx{{ entry['IDX'] }}"
-                  href="javascript:void(0)" onclick="show('idx{{ entry['IDX'] }}')">
-                  BibTeX
-                </a>
-                {{ entry['EXTRA'] | safe }}
-                <div id="idx{{ entry['IDX'] }}" class="dynamic_link">
-                  <pre>
-@{{types[entry['TYPE']][0] }}{ {{- entry['CITEKEY'] }},
-    author    = { {{- entry['AUTH-BIB'] -}} },
-    title     = { {{- entry['TITLE'] -}} },
-    {{- types[entry['TYPE']][1] -}} = { {{- entry['VENUE'] -}} },
-    year      = { {{- entry['YEAR'] -}} },
-    url       = { {{- entry['URL'] -}} },
-}
-                  </pre>
-                </div>
-            </div> <!-- text -->
-          </div>
-        </div>
-      </div> <!-- body -->
-    </div> <!-- card -->
-    """
-    template = Template(template_string)
-    return template.render(entry=entry, colors=colors, types=types)
-
-def create_publication_entry(entry):
+def update_pub_entry(entry):
   # Create a Paper button
   if "URL" in entry and len(entry["URL"].strip()) > 0:
     entry["PAPER-BUTTON"] = f"<a class=\"sbtn\" href={entry['URL']}>Paper</a>"
@@ -105,7 +56,7 @@ def create_publication_entry(entry):
 
   # Before doing pretification
   entry["CITEKEY"] = entry["VENUE-ACR"].replace(' ','-') + ":" + entry["AUTHORS"][0].split()[-1] + str(entry["YEAR"])
-# if current student, add photo
+  # if current student, add photo
   for author in entry["AUTHORS"]:
     if author in student_names:
       name = author.replace(" ","")
@@ -117,7 +68,6 @@ def create_publication_entry(entry):
     else:
       entry["STUDENTPHOTO"] = ""
 
-
   # Authors
   entry["AUTHORS"] = pretty_author_names(", ".join(entry["AUTHORS"]))
 
@@ -128,16 +78,13 @@ def create_publication_entry(entry):
     entry["AUTHORS"] = entry["AUTHORS"].replace(p, f"<div class=\"name\">{p}</div>")
   entry["AUTHORS"] = entry["AUTHORS"].replace("Yonatan Bisk", f"<div class=\"name\">Yonatan Bisk</div>")
 
-
-    # Include extra links
+  # Include extra links
   entry["EXTRA"] = ""
   btn = "&nbsp; <a class=\"sbtn\" "
   if "EXTRAS" in entry:
     for key in entry["EXTRAS"]:
       link = entry["EXTRAS"][key]
       entry["EXTRA"] += " {} href=\"{}\">{}</a>".format(btn, link, key)
-
-  return publication_html(entry)
 
 def create_latex_entry(entry):
   authors = ", ".join(entry["AUTHORS"])
@@ -159,41 +106,11 @@ def create_latex_entry(entry):
   template = Template(template_string)
   return template.render(entry=entry, authors=authors)
   
-
-def student_card_html(entry):
-    template_string = """
-        <!-- {{ entry['NAME'] }} -->
-        <div class="col card student" style="max-width: 270px; min-width:200px;">
-          <a href="{{ entry['WEB'] }}">
-            <img src=images/students/{{ entry['PIC'] }} class="card-img-top" alt="{{ entry['NAME'] }}">
-          </a>
-          <img src="images/{{ entry['DEPT'] }}.png" height=40px class="logo" alt="{{ entry['DEPT'] }} logo">
-          <div class="card-body">
-            <h5 class="card-title">
-              <p class="m-flip js-flip" style="font-weight:200; font-size: 20px;">
-                <span class="m-flip_item"><a href="{{ entry['WEB'] }}"><b>{{ entry['NAME'] }}</b></a></span>
-                <span class="m-flip_item"><a href="{{ entry['WEB'] }}"><b>{{ entry['NATIVE'] }}</b></a></span>
-              </p>
-              <h6><a href="https://twitter.com/{{ entry['X'] }}">@{{ entry['X'] }}</a>
-                  <font class="pronoun">{{ entry['PRON'] }}</font></h6>
-              <img width=50 src="images/WSs/WS{{ entry['WS'] }}_small.webp" 
-                  style="margin-bottom:0px;margin-top:0px;width:50;border:0px;display:inline;float:right;" 
-                  alt="small robot">
-            </h5>
-            <p class="card-text"><br><h6>{{ entry['COADVISOR'] }} </h6></p>
-            <hr>
-            <h6>{{ entry['RESEARCH'] }}</h6>
-          </div> <!-- card-body -->
-        </div> <!-- card -->
-    """
-    template = Template(template_string)
-    return template.render(entry=entry)
-
-def create_student_card(S):
+def update_student_card(S):
     if "CO" in S:
-        S["COADVISOR"] = f"<h6>(co- <a href=\"{S['CO-WEB']}\">{S['CO']}</a>)</h6>"
+        S["COADVISOR"] = f"<div class='coadvisor'>(co- <a href=\"{S['CO-WEB']}\">{S['CO']}</a>)</div>"
     else:
-        S["COADVISOR"] = "<h6>&nbsp;</h6>"
+        S["COADVISOR"] = "<div class='coadvisor'>&nbsp;</div>"
 
     if "NATIVE" not in S: 
        S["NATIVE"] = S["NAME"]
@@ -211,8 +128,6 @@ def create_student_card(S):
         collapse += "</div>"
     papers = collapse
     S["RESEARCH"] = papers
-    return student_card_html(S)
-
 
 ## Generate Publications Website ##
 with open("pub_template.jinja2", 'r') as file:
@@ -222,8 +137,7 @@ idx = len(pubs)
 peer_reviewed = ""
 for entry in pubs:
   entry["IDX"] = idx
-  generated_html = create_publication_entry({v:entry[v] for v in entry})
-  peer_reviewed += generated_html
+  update_pub_entry(entry)
   idx -= 1
 
 ## Generate Plot ##
@@ -232,7 +146,7 @@ for entry in pubs:
     u = 1.0
     data[entry["FIELD"]] += u
 
-rendered = pub_template.render(PEERREVIEW=peer_reviewed, data=data)
+rendered = pub_template.render(publications=pubs, data=data, colors=colors, types=types)
 with open("../publications.html", 'wt') as output_file:
     output_file.write(rendered)
 
@@ -272,16 +186,11 @@ os.system("pdflatex CV.tex")
 os.system("rm CV.tex CV.log CV.aux CV.out")
 os.system("mv CV.pdf ../")
 
-
 ## Generate group page
 with open("group_template.jinja2", 'r') as file:
   group_template = Template(file.read())
-block = '<div class="container" id="students"><div class="row row-cols-1 row-cols-3 g-0">'
-students = [create_student_card(stud) for stud in student_yaml]
-for student in students:
-  block += student + "\n\n"
-block += '</div></div>' 
-
-group_render = group_template.render(PHD=block)
+for stud in student_yaml:
+    update_student_card(stud)
+group_render = group_template.render(students=student_yaml)
 with open("../CLAW/index.html", 'wt') as output_file:
     output_file.write(group_render)
