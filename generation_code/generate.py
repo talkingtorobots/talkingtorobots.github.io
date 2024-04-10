9dd4a5f02e7dd21ec45aae34e41d43c41ea511fc
Yonatan Bisk <ybisk@yonatanbisk.com>
2024-04-09 20:36:51 -0400

    moving even more logic into jinja

--- generation_code/generate.py
+++ generation_code/generate.py
@@ -44,15 +44,6 @@ def pretty_author_names(s):
   return s
 
 def update_pub_entry(entry):
-  # Create a Paper button
-  if "URL" in entry and len(entry["URL"].strip()) > 0:
-    entry["PAPER-BUTTON"] = f"<a class=\"sbtn\" href={entry['URL']}>Paper</a>"
-  else:
-    entry["PAPER-BUTTON"] = ""
-
-  # LaTeX compatible 
-  entry["AUTH-BIB"] = ' and '.join(entry['AUTHORS'])
-
   # Before doing pretification
   entry["CITEKEY"] = entry["VENUE-ACR"].replace(' ','-') + ":" + entry["AUTHORS"][0].split()[-1] + str(entry["YEAR"])
   # if current student, add photo
@@ -68,46 +59,21 @@ def update_pub_entry(entry):
       entry["STUDENTPHOTO"] = ""
 
   # Authors
-  entry["AUTHORS"] = pretty_author_names(", ".join(entry["AUTHORS"]))
-  entry["AUTHORS-WEB"] = entry["AUTHORS"]
+  entry["AUTHORS-PRETTY"] = pretty_author_names(", ".join(entry["AUTHORS"]))
 
   # Add links to all co-authors
   for v in webs:
-    entry["AUTHORS-WEB"] = entry["AUTHORS-WEB"].replace(v, "<a href={}>{}</a>".format(webs[v], v))
+    entry["AUTHORS-PRETTY"] = entry["AUTHORS-PRETTY"].replace(v, "<a href={}>{}</a>".format(webs[v], v))
   for p in student_names:
-    entry["AUTHORS-WEB"] = entry["AUTHORS-WEB"].replace(p, f"<div class=\"name\">{p}</div>")
-  entry["AUTHORS-WEB"] = entry["AUTHORS-WEB"].replace("Yonatan Bisk", f"<div class=\"name\">Yonatan Bisk</div>")
+    entry["AUTHORS-PRETTY"] = entry["AUTHORS-PRETTY"].replace(p, f"<div class=\"name\">{p}</div>")
+  entry["AUTHORS-PRETTY"] = entry["AUTHORS-PRETTY"].replace("Yonatan Bisk", f"<div class=\"name\">Yonatan Bisk</div>")
 
-  # Include extra links
-  entry["EXTRA"] = ""
-  btn = "&nbsp; <a class=\"sbtn\" "
-  if "EXTRAS" in entry:
-    for key in entry["EXTRAS"]:
-      link = entry["EXTRAS"][key]
-      entry["EXTRA"] += " {} href=\"{}\">{}</a>".format(btn, link, key)
-
-def update_student_card(S):
-    if "CO" in S:
-        S["COADVISOR"] = f"<div class='coadvisor'>(co- <a href=\"{S['CO-WEB']}\">{S['CO']}</a>)</div>"
-    else:
-        S["COADVISOR"] = "<div class='coadvisor'>&nbsp;</div>"
-
-    if "NATIVE" not in S: 
-       S["NATIVE"] = S["NAME"]
+def add_student_papers(S):
     # This is inefficient but whatevs
-    papers = ""
+    S['RESEARCH'] = []
     for pub in pubs:
         if S["NAME"] in pub["AUTHORS"] and pub["TYPE"] != "workshop" :
-            papers += f"<a href=\"{pub['URL']}\">{pub['TITLE']}</a><br><br>\n\n"
-
-    collapse = ""
-    if len(papers) > 0:
-        collapse = f"<button class=\"collapsible\">Research</button>\n"
-        collapse += "<div class=\"content\">\n"
-        collapse += "<p style='margin-top:1rem;margin-bottom:0;'>" + papers + "</p>"
-        collapse += "</div>"
-    papers = collapse
-    S["RESEARCH"] = papers
+            S['RESEARCH'].append(pub)
 
 ## Generate Publications Website ##
 with open("templates/pub_template.jinja2", 'r') as file:
@@ -141,7 +107,7 @@ latex_papers = {"Journal": [],
                 }
 previous = 10000
 for entry in pubs:
-  entry["AUTHORS"] = entry["AUTHORS"].replace("Yonatan Bisk","\\YB{}")
+  entry["AUTHORS"] = ", ".join(entry["AUTHORS"]).replace("Yonatan Bisk","\\YB{}")
   if "NOTE" in entry:
     entry["VENUE"] += "\\\\ -- \\textbf{" + entry["NOTE"] + "}"
   latex_papers[entry["TYPE"]].append(entry)
@@ -173,7 +139,7 @@ os.system("mv CV.pdf ../")
 with open("templates/group_template.jinja2", 'r') as file:
   group_template = Template(file.read())
 for stud in student_yaml:
-    update_student_card(stud)
+    add_student_papers(stud)
 masters = yaml.load(open("yaml/masters_interns.yaml"), Loader=yaml.CLoader)
 alumni = yaml.load(open("yaml/alumni.yaml"), Loader=yaml.CLoader)
 group_render = group_template.render(students=student_yaml, alumni=alumni, masters=masters)
